93c307f0097408eb3eb8ab9199132a8820f831c3
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2025-03-20 14:38:13 +0800

    refactor(Map): 移动 Map 类中的角色生成逻辑到 MapGen.cs
    - 将 SpawnPlayer 和 SpawnNPC 方法从 Map 类中移除
    - 在 MapGen 类中添加新的 SpawnPlayer 和 SpawnNPC 方法
    - 更新角色生成逻辑，使用新的 MapGen 类方法
    - 优化代码结构，使角色生成逻辑与地图生成逻辑分离

--- Assets/Scripts/MapGen.cs
+++ Assets/Scripts/MapGen.cs
@@ -13,23 +13,50 @@ public class MapGen : MonoBehaviour
     
 
     private GameObject _camera;
-    private AssetDatabaseLoader assetDatabaseLoader;
-    private GameTime gameTime;
+    private AssetDatabaseLoader _assetDatabaseLoader;
+    private GameTime _gameTime;
 
     public void Start()
     {
         // 引用
-        assetDatabaseLoader = GameObject.Find("AssetDatabaseLoader").GetComponent<AssetDatabaseLoader>();
+        _assetDatabaseLoader = GameObject.Find("AssetDatabaseLoader").GetComponent<AssetDatabaseLoader>();
         _camera = GameObject.Find("Main Camera");
-        playerPrefab = assetDatabaseLoader.Stickman;
-        gameTime = GameObject.Find("GameTime").GetComponent<GameTime>();
+        playerPrefab = _assetDatabaseLoader.Stickman;
+        _gameTime = GameObject.Find("GameTime").GetComponent<GameTime>();
         
-        map = new Map(0, height, width, tilemap, assetDatabaseLoader);
+        map = new Map(0, height, width, tilemap, _assetDatabaseLoader);
         map.GenerateMap();
-        map.DrawMap();
         map.GeneratePassMap();
-        player = map.SpawnPlayer(playerPrefab, gameTime);
-        map.SpawnNPC(assetDatabaseLoader.LittleBJY, gameTime);
+        map.DrawMap();
+        SpawnPlayer(playerPrefab, _gameTime);
+        SpawnNPC(_assetDatabaseLoader.LittleBJY, _gameTime);
         _camera.GetComponent<CameraMove>().MoveTo(player.GetComponent<Capability>().cellPosition, tilemap);
+    }
+    
+    public void SpawnPlayer(GameObject playerPrefab, GameTime gameTime)
+    {
+        player = Instantiate(playerPrefab);
+        var capability = player.GetComponent<Capability>();
+        gameTime.allCharacters.Add(player); // 将player添加到游戏时间管理器中
+        capability.attitude = Attitude.Player;
+        capability.name = "Player";
+        var spawnPos = map.RandomCellPos();
+        capability.cellPosition = spawnPos;
+        map.passMap[spawnPos.x][spawnPos.y] = 1;
+        // __camera.GetComponent<CameraMove>().MoveTo(tilemap.GetCellCenterWorld(spawnPos));
+    }
+    
+    
+    public void SpawnNPC(GameObject NPCPrefab, GameTime gameTime)
+    {
+        var npc = Instantiate(NPCPrefab);
+        // if (npc == null) Debug.LogError("NPC生成失败");
+        var capability = npc.GetComponent<Capability>();
+        gameTime.allCharacters.Add(npc);
+        capability.attitude = Attitude.Neutral;
+        var spawnPos = map.RandomCellPos();
+        map.passMap[spawnPos.x][spawnPos.y] = 1;
+        capability.cellPosition = spawnPos;
+        capability.SetNextActionTime(1f, gameTime);
     }
 }
